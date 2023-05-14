79b49d6cc8ea013e6e362d29562995669bcf04bc
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2023-05-14 16:21:38 +0900

    windows: build with Qt 6.5

--- libs/deploy_windows64.sh
+++ libs/deploy_windows64.sh
@@ -13,6 +13,9 @@ cp $BUILD/nekoray.exe $DEST
 pushd $DEST
 windeployqt nekoray.exe --no-compiler-runtime --no-system-d3d-compiler --no-opengl-sw --verbose 2
 rm -rf translations
+rm -rf libEGL.dll libGLESv2.dll Qt6Pdf.dll
+curl -fLSO https://github.com/MatsuriDayo/nekoray_qt_runtime/releases/download/20220503/libcrypto-3-x64.dll
+curl -fLSO https://github.com/MatsuriDayo/nekoray_qt_runtime/releases/download/20220503/libssl-3-x64.dll
 popd
 
 #### prepare deployment ####
