d769723b237d9b6b1a4f6440d0ffa4a68dc30b15
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2023-05-15 11:09:01 +0900

    fix ui

--- ui/widget/FloatCheckBox.h
+++ ui/widget/FloatCheckBox.h
@@ -1,16 +1,33 @@
 #pragma once
 
+#include <QEvent>
 #include <QCheckBox>
 
 class FloatCheckBox : public QCheckBox {
 public:
-    explicit FloatCheckBox(QWidget *parent, QWidget *window) : QCheckBox(window) {
+    QWidget *parent;
+    QWidget *window;
+
+    void refresh() {
         setFixedSize(24, 24);
         auto pos = parent->rect().topRight();
         pos = parent->mapTo(window, pos);
         pos.setX(pos.x() - 48); // ?
         move(pos);
         raise();
-        if (parent->isVisible()) show();
+        setVisible(parent->isVisible());
+    };
+
+    bool eventFilter(QObject *obj, QEvent *e) override {
+        if (obj != window || e->type() != QEvent::Resize) return false;
+        refresh();
+        return false;
+    };
+
+    explicit FloatCheckBox(QWidget *parent, QWidget *window) : QCheckBox(window) {
+        this->parent = parent;
+        this->window = window;
+        window->installEventFilter(this);
+        refresh();
     };
 };
