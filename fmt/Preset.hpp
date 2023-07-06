5d94f97961093beb5b44054129424968d40f871f
tdjisdj <125038375+tdjisdj@users.noreply.github.com>
2023-07-06 15:40:25 +0800

    change Preset

--- fmt/Preset.hpp
+++ fmt/Preset.hpp
@@ -3,7 +3,8 @@
 namespace Preset {
     namespace Xray {
         inline QStringList UtlsFingerPrint = {"", "chrome", "firefox", "edge", "safari", "360", "qq", "ios", "android", "random", "randomized"};
-        inline QStringList ShadowsocksMethods = {"2022-blake3-aes-128-gcm", "2022-blake3-aes-256-gcm", "2022-blake3-chacha20-poly1305", "aes-256-gcm", "aes-128-gcm", "chacha20-poly1305", "none"};
+        inline QStringList ShadowsocksMethods = {"2022-blake3-aes-128-gcm", "2022-blake3-aes-256-gcm", "2022-blake3-chacha20-poly1305", "aes-256-gcm", "aes-128-gcm", "chacha20-poly1305", "xchacha20-ploy1305", "none"};
+        inline QStringList Flows = {"xtls-rprx-vision", "xtls-rprx-vision-udp443"};
     } // namespace Xray
 
     namespace SingBox {
@@ -11,6 +12,7 @@ namespace Preset {
         inline QStringList DomainStrategy = {"", "ipv4_only", "ipv6_only", "prefer_ipv4", "prefer_ipv6"};
         inline QStringList UtlsFingerPrint = {"", "chrome", "firefox", "edge", "safari", "360", "qq", "ios", "android", "random", "randomized"};
         inline QStringList ShadowsocksMethods = {"2022-blake3-aes-128-gcm", "2022-blake3-aes-256-gcm", "2022-blake3-chacha20-poly1305", "none", "aes-128-gcm", "aes-192-gcm", "aes-256-gcm", "chacha20-ietf-poly1305", "xchacha20-ietf-poly1305", "aes-128-ctr", "aes-192-ctr", "aes-256-ctr", "aes-128-cfb", "aes-192-cfb", "aes-256-cfb", "rc4-md5", "chacha20-ietf", "xchacha20"};
+        inline QStringList Flows = {"xtls-rprx-vision"};
     } // namespace SingBox
 
     namespace Windows {
