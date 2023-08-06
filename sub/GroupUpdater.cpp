61b1754495776ac7562c614f1109e3a5fe205817
2022-blake3-chacha8-poly1305 <139959885+xchacha20-ietf-poly1305@users.noreply.github.com>
2023-08-06 09:06:28 +0800

    feat(sub): get packet encoding from Clash.Meta's sub

--- sub/GroupUpdater.cpp
+++ sub/GroupUpdater.cpp
@@ -250,7 +250,7 @@ namespace NekoGui_sub {
                             if (!path.isEmpty()) ssPlugin << "path=" + path;
                             // clash only: skip-cert-verify
                             // clash only: headers
-                            // clash: mux=?
+                             // clash: mux=?
                         }
                         bean->plugin = ssPlugin.join(";");
                     }
@@ -270,11 +270,11 @@ namespace NekoGui_sub {
                     if (type == "vless") {
                         bean->flow = Node2QString(proxy["flow"]);
                         bean->password = Node2QString(proxy["uuid"]);
-                        // meta vless xudp
-                        auto xudp = proxy["xudp"];
-                        if (xudp.IsDefined() && !xudp.IsNull() && Node2Bool(xudp) == false) {
-                            bean->stream->packet_encoding = "";
+                        // meta packet encoding
+                        if (Node2Bool(proxy["PacketAddr"])) {
+                            bean->stream->packet_encoding = "packet";
                         } else {
+                            // For VLESS, default to use xudp
                             bean->stream->packet_encoding = "xudp";
                         }
                     } else {
@@ -322,6 +322,10 @@ namespace NekoGui_sub {
                     bean->stream->alpn = Node2QStringList(proxy["alpn"]).join(",");
                     if (Node2Bool(proxy["tls"])) bean->stream->security = "tls";
                     if (Node2Bool(proxy["skip-cert-verify"])) bean->stream->allow_insecure = true;
+
+                    // meta packet encoding
+                    if (Node2Bool(proxy["XUDP"])) bean->stream->packet_encoding = "xudp";
+                    if (Node2Bool(proxy["PacketAddr"])) bean->stream->packet_encoding = "packet";
 
                     // opts
                     auto ws = NodeChild(proxy, {"ws-opts", "ws-opt"});
