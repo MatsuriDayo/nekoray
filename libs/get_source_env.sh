4b9ff1dcd2bffed1aa19443872896770441e0862
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2023-08-17 20:49:37 +0900

    3.17

--- libs/get_source_env.sh
+++ libs/get_source_env.sh
@@ -1,5 +1,5 @@
 if [ ! -z $ENV_NEKORAY ]; then
-  export COMMIT_SING_BOX_EXTRA="c419bc9a28b23f528072aedbb2d956dbfda0cbb3"
+  export COMMIT_SING_BOX_EXTRA="e9294cd60a5ad5e1efec2fbbe35be03f057c2a63"
   export COMMIT_MATSURI_XRAY="f6843e4e1442341e88ac34c09ca2d243cdcc09f6"
 fi
 
